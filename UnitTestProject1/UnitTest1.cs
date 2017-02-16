451e7cd77c8ebfdbe8ec7cc34aced4a838a022f2
Madoka Chiyoda <chomado@gmail.com>
2017-02-16 20:48:03 +0900

    enable live unit testing

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LiveUnitTestSample;
 
 namespace UnitTestProject1
 {
@@ -9,6 +10,23 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            var answer = Calc.Add(2, 3);
+
+            // 期待される値と、実際の値を比較し、
+            // 一致したら「テスト成功」（ユニットテスト）
+            Assert.AreEqual(expected: 5, actual: answer);
+        }
+
+        // "tesm" でテストメソッドのスニペット (tab+tab)
+        [TestMethod]
+        public void AddTest()
+        {
+            var answer = Calc.Add(4, 5);
+
+            // 期待される値と、実際の値を比較し、
+            // 一致したら「テスト成功」（ユニットテスト）
+            Assert.AreEqual(expected: 9, actual: answer);
+
         }
     }
 }
